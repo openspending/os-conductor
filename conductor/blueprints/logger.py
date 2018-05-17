f3b94b2d477bfe36071158bbbce703e9f74bdcc6
Adam Kariv <adam.kariv@gmail.com>
2018-05-17 17:59:07 +0300

    Reduce ES logging

--- conductor/blueprints/logger.py
+++ conductor/blueprints/logger.py
@@ -7,3 +7,8 @@ formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 handlr.setFormatter(formatter)
 logger.addHandler(handlr)
 logger.setLevel(logging.INFO)
+
+es_logger = logging.getLogger('elasticsearch')
+es_logger.setLevel(logging.INFO)
+
+
